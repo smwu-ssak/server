dac064e50d4f7e088cae80a72a677277c007f209
김진희 <jin03hee16@gmail.com>
2019-11-18 20:28:01 +0900

    feat: 장바구니 담기, 조회 완성

---
+++ Seed_server/routes/basket/basket.js
@@ -0,0 +1,125 @@
+var express = require('express');
+var router = express.Router();
+
+const resMessage = require('../../module/utils/responseMessage');
+const statusCode = require('../../module/utils/statusCode');
+const util = require('../../module/utils/utils');
+
+const pool = require('../../module/pool');
+const jwt = require('../../module/jwt');
+
+//장바구니 조회
+router.get('/', async (req, res) => {
+  const user = jwt.verify(req.headers.token);
+  console.log("idx::" + user.idx);
+
+  let basketQuery =
+    `
+    SELECT p.name, p.quantity, p.image, p.salePrice
+    FROM Product AS p 
+    JOIN Basket AS b 
+    ON p.idProduct = b.product_id 
+    WHERE b.user_id = ?;
+    `;
+
+  try {
+    if (user.idx == undefined) {
+      res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.INVALID_TOKEN));
+    }
+
+    const selectResult = await pool.queryParam_Parse(basketQuery, [user.idx]);
+    if (!selectResult) {
+      res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.READ_FAIL));
+    }
+    else {
+      res.status(200).send(util.successTrue(statusCode.OK, resMessage.READ_SUCCESS, selectResult));
+    }
+  } catch (err) {
+    console.log("Select Basket Error => " + err);
+  }
+
+});
+
+//장바구니 담기
+router.get('/:idProduct', async (req, res) => {
+  const user = jwt.verify(req.headers.token);
+  console.log("body:::" + JSON.stringify(req.body));
+
+  const dupProduct =
+    `
+  SELECT product_id
+  FROM Basket
+  WHERE product_id = ?;
+  `;
+  let basketQuery =
+    `
+    INSERT INTO 
+    Basket (user_id, product_id) 
+    VALUES (?, ?) ;
+    `;
+
+  try {
+    if (user == undefined) {
+      res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.SAVE_FAIL));
+    }
+
+    const selectResult = await pool.queryParam_Parse(dupProduct, [req.params.idProduct]);
+    if (selectResult[0] == null) {
+      const insertResult = await pool.queryParam_Parse(basketQuery, [user.idx, req.params.idProduct]);
+      if (!insertResult) {
+        res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.SAVE_FAIL));
+      }
+      else {
+        res.status(200).send(util.successTrue(statusCode.OK, resMessage.SAVE_SUCCESS));
+      }
+    } else {
+      res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.ALREADY_BASKET));
+    }
+
+  } catch (err) {
+    console.log("Insert Basket Error => " + err);
+  }
+});
+
+//장바구니 비우기
+router.post('/', async (req, res) => {
+  const user = jwt.verify(req.headers.token);
+  console.log("body:::" + JSON.stringify(req.body));
+
+  const dupProduct =
+    `
+  SELECT product_id
+  FROM Basket
+  WHERE product_id = ?;
+  `;
+  let basketQuery =
+    `
+    INSERT INTO 
+    Basket (user_id, product_id) 
+    VALUES (?, ?) ;
+    `;
+
+  try {
+    if (user == undefined) {
+      res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.SAVE_FAIL));
+    }
+
+    const selectResult = await pool.queryParam_Parse(dupProduct, [req.params.idProduct]);
+    if (selectResult[0] == null) {
+      const insertResult = await pool.queryParam_Parse(basketQuery, [user.idx, req.params.idProduct]);
+      if (!insertResult) {
+        res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.SAVE_FAIL));
+      }
+      else {
+        res.status(200).send(util.successTrue(statusCode.OK, resMessage.SAVE_SUCCESS));
+      }
+    } else {
+      res.status(200).send(util.successFalse(statusCode.BAD_REQUEST, resMessage.ALREADY_BASKET));
+    }
+
+  } catch (err) {
+    console.log("Insert Basket Error => " + err);
+  }
+});
+
+module.exports = router;
